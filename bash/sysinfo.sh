c79a89286582d1e116dcb6df2580f21d71d0f95c
nikhilmankotia <2001nikman@gmail.com>
2022-09-28 13:25:09 -0400

    sysinfo.sh lab-1

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#AdministrativeScryptingLab1
+#First scrypting assignment
+echo "Hostname"
+echo "pc200496519"
+#First Command showing the system information
 hostnamectl
-ip address 
+#To have the ip version4 address of the working computer
+echo "IP VERSION 4"
+ip a s ens33 | grep -w inet | awk '{print $2}'
+#Command to have ipv6 address of the working computer
+echo "IP VERSION 6"
+ip a s ens33 | grep -w inet6 | awk '{print $2}'
+#Command showing the avialable and total space in the system
 df
+#end of the script
