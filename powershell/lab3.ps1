46c9813024bdbe6a773044f567b9573f7bc75357
nikhilmankotia <2001nikman@gmail.com>
2022-12-13 12:37:40 -0500

    Create lab3.ps1
    lab3.ps1

---
+++ powershell/lab3.ps1
@@ -0,0 +1,2 @@
+﻿Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration | ? {$_.IPEnabled -eq "True"} | #Where {$_.ipenabled -eq "True" } |
+Format-Table -AutoSize Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
